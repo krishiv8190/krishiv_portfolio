0b5a2d3e8702290e51a974043b0c488327b12dbe
Krishiv <avin.pandita32@gmail.com>
2026-08-12 07:32:44 +0530

    Implement Navbar functionality

---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+const navLinks = [
+  {
+    id: 1,
+    name: "Portfolio",
+  },
+  {
+    id: 2,
+    name: "Contact",
+  },
+  {
+    id: 3,
+    name: "Projects",
+  },
+];
+
+const navIcons = [
+  {
+    id: 1,
+    img: "/icons/wifi.svg",
+  },
+  {
+    id: 2,
+    img: "/icons/search.svg",
+  },
+  {
+    id: 3,
+    img: "/icons/user.svg",
+  },
+  {
+    id: 4,
+    img: "/icons/mode.svg",
+  },
+];
+export { navLinks, navIcons };
